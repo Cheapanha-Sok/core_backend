6ae5c36acee2f0e051545f17f52519fbc0de778e
sokcheapanha <sokcheapanha11@gmail.com>
2024-07-31 11:29:49 +0700

    update BaseEntity

--- src/main/java/com/panha/base/BaseEntity.java
+++ src/main/java/com/panha/base/BaseEntity.java
@@ -1,14 +1,18 @@
 package com.panha.base;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.util.Date;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @CreatedDate
