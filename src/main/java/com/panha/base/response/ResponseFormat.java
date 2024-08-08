fc788b657933bbff6d7bf715c57a3f14c46c9e6e
sokcheapanha <sokcheapanha11@gmail.com>
2024-08-08 21:48:55 +0700

    Generic response

---
+++ src/main/java/com/panha/base/response/ResponseFormat.java
@@ -0,0 +1,18 @@
+package com.panha.base.response;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+
+public interface ResponseFormat {
+    ResponseDTO respondCustomStatus(Object data, HttpStatus status, String message);
+
+    ResponseDTO respondDynamic(Object data, HttpStatus status, String message, Long total);
+
+    ResponseDTO respondID(Object data, HttpStatus status, String message);
+
+    ResponseDTO respondList(java.util.List<Object> data, HttpStatus status, String message);
+
+    ResponseDTO respondObj(Object data, HttpStatus status, String message);
+
+    <T> ResponseDTO respondPage(Page<T> data, HttpStatus status, String message);
+}
