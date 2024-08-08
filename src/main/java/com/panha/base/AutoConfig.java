ef944c5814ec06e9150d694b5263eedfe9cb7cdc
sokcheapanha <sokcheapanha11@gmail.com>
2024-08-08 23:04:38 +0700

    add component scan

---
+++ src/main/java/com/panha/base/AutoConfig.java
@@ -0,0 +1,10 @@
+package com.panha.base;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AutoConfig {
+}
